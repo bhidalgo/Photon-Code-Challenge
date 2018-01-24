d2848d0ff21e3570bd8b2b58351a7d2031798add
Brandon Hidalgo <brandontheprogrammer@bitbucket.org>
2018-01-24 17:26:06 -0500

    [insert]
    MainActivity now overrides POLCView and contains a fragment container in the layout file.
    
    Created a fragment to handle accepting matrix dimensions and made MainActivity add it to its fragment container on creation.
    
    added dependencies for support library and design.

--- app/src/main/java/hidalgo/brandon/photoncodechallenge/MainActivity.java
+++ app/src/main/java/hidalgo/brandon/photoncodechallenge/MainActivity.java
@@ -1,13 +1,53 @@
 package hidalgo.brandon.photoncodechallenge;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import hidalgo.brandon.photoncodechallenge.view.POLCView;
+
+public class MainActivity extends AppCompatActivity implements POLCView, MatrixDimensFragment.MatrixDimensFragmentListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main_activity);
+
+        initializeMatrixDimensFragment();
+    }
+
+    @Override
+    public void createMatrixWithDimens(int rows, int cols) {
+        Toast.makeText(this, "FAB Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showFailure(int cost, int[] path) {
+
+    }
+
+    @Override
+    public void showMatrix(int[][] matrix) {
+
+    }
+
+    @Override
+    public void showPOLC(int path) {
+
+    }
+
+    @Override
+    public void showSuccess(int cost, int[] path) {
+
+    }
+
+    private void initializeMatrixDimensFragment() {
+        MatrixDimensFragment matrixDimensFragment = MatrixDimensFragment.getInstance();
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        fm.beginTransaction().add(R.id.fragmentContainer, matrixDimensFragment).commit();
     }
 }
---
+++ app/src/main/java/hidalgo/brandon/photoncodechallenge/MatrixDimensFragment.java
@@ -0,0 +1,81 @@
+package hidalgo.brandon.photoncodechallenge;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.NumberPicker;
+
+import hidalgo.brandon.photoncodechallenge.databinding.MatrixDimensFragmentBinding;
+
+public class MatrixDimensFragment extends Fragment {
+    private MatrixDimensFragmentListener listener;
+
+    private NumberPicker mRowsNumberPicker;
+
+    private NumberPicker mColumnsNumberPicker;
+
+    public interface MatrixDimensFragmentListener {
+        void createMatrixWithDimens(int rows, int cols);
+    }
+
+    public MatrixDimensFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try{
+            listener = (MatrixDimensFragmentListener) context;
+        }
+        catch(ClassCastException e) {
+            Log.e("MatrixDimensFrag", "Context must implement the MatrixDimensFragmentListener.");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        MatrixDimensFragmentBinding binding = DataBindingUtil.inflate(inflater, R.layout.matrix_dimens_fragment, container, false);
+
+        View mainView = binding.getRoot();
+
+        setUpNumberPickers(mainView);
+
+        binding.setFragment(this);
+
+        return mainView;
+    }
+
+    public static MatrixDimensFragment getInstance() {
+        return new MatrixDimensFragment();
+    }
+
+    public void handleOnNextClicked(View view) {
+        int rows = mRowsNumberPicker.getValue();
+
+        int cols = mColumnsNumberPicker.getValue();
+
+        listener.createMatrixWithDimens(rows, cols);
+    }
+
+    private void setUpNumberPickers(View parentView) {
+        mRowsNumberPicker = parentView.findViewById(R.id.rowsNumberPicker);
+
+        mRowsNumberPicker.setMinValue(1);
+
+        mRowsNumberPicker.setMaxValue(10);
+
+        mColumnsNumberPicker = parentView.findViewById(R.id.columnsNumberPicker);
+
+        mColumnsNumberPicker.setMinValue(1);
+
+        mColumnsNumberPicker.setMaxValue(10);
+    }
+}
