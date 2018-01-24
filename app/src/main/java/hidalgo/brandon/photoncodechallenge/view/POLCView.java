69b783837abda6d227763025fefb9dd507766cea
Brandon Hidalgo <brandontheprogrammer@bitbucket.org>
2018-01-24 14:50:54 -0500

    [insert]
    added a method to show a given matrix.

--- app/src/main/java/hidalgo/brandon/photoncodechallenge/view/POLCView.java
+++ app/src/main/java/hidalgo/brandon/photoncodechallenge/view/POLCView.java
@@ -7,6 +7,8 @@ package hidalgo.brandon.photoncodechallenge.view;
 public interface POLCView {
     void showFailure(int cost, int[] path);
 
+    void showMatrix(int[][] matrix);
+
     void showPOLC(int path);
 
     void showSuccess(int cost, int[] path);
