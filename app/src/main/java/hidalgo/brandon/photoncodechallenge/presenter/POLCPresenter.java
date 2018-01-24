03c94d720ae2ba2d21e08e55b247aa7b314cc07a
Brandon Hidalgo <brandontheprogrammer@bitbucket.org>
2018-01-24 16:03:38 -0500

    [delete]
    removed generated comments

--- app/src/main/java/hidalgo/brandon/photoncodechallenge/presenter/POLCPresenter.java
+++ app/src/main/java/hidalgo/brandon/photoncodechallenge/presenter/POLCPresenter.java
@@ -1,9 +1,5 @@
 package hidalgo.brandon.photoncodechallenge.presenter;
 
-/**
- * Created by Brandon Hidalgo on 1/22/2018.
- */
-
 public interface POLCPresenter {
     void computePOLC(int[][] matrix);
 }
--- app/src/main/java/hidalgo/brandon/photoncodechallenge/view/POLCView.java
+++ app/src/main/java/hidalgo/brandon/photoncodechallenge/view/POLCView.java
@@ -1,9 +1,5 @@
 package hidalgo.brandon.photoncodechallenge.view;
 
-/**
- * Created by Brandon Hidalgo on 1/22/2018.
- */
-
 public interface POLCView {
     void showFailure(int cost, int[] path);
 
