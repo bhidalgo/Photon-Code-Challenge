7aa8658ba2aedaf3a1d00180bc2affb324003873
Brandon Hidalgo <brandontheprogrammer@bitbucket.org>
2018-01-31 23:21:03 -0500

    [insert]
    added hamcrest matcher for a matrix displaying a certain amount of rows and a certain amount of columns.

--- app/src/androidTest/java/hidalgo/brandon/photoncodechallenge/Matchers/MatrixMatchers.java
+++ app/src/androidTest/java/hidalgo/brandon/photoncodechallenge/Matchers/MatrixMatchers.java
@@ -2,6 +2,7 @@ package hidalgo.brandon.photoncodechallenge.Matchers;
 
 import android.support.test.espresso.matcher.BoundedMatcher;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -30,6 +31,27 @@ public class MatrixMatchers {
             @Override
             protected boolean matchesSafely(MatrixView item) {
                 return item.getRows() == rows && item.getColumns() == columns;
+            }
+        };
+    }
+
+    /**
+     * Matches against a MatrixView containing the amount of displayed rows and columns
+     *
+     * @param rows    the expected number of displayed rows
+     * @param columns the expected number of displayed columns
+     * @return boolean
+     */
+    public static Matcher<View> displaysRowsAndColumns(final int rows, final int columns) {
+        return new BoundedMatcher<View, MatrixView>(MatrixView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("displays rows: " + rows + " and columns: " + columns);
+            }
+
+            @Override
+            protected boolean matchesSafely(MatrixView item) {
+                return item.getChildCount() == rows && ((LinearLayout) item.getChildAt(0)).getChildCount() == columns;
             }
         };
     }
